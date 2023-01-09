e2ff42809f263322df8b6a5054bc0555b9c10679
Sumit-dey <deysumit417@gmail.com>
2023-01-09 13:06:15 -0500

    aa

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -5,6 +5,10 @@ import java.util.*;
 public class E01 {
     public int getNthFib(int n) {
         // FIXME
-        return -1;
+    return func(n-1);
+    }
+    public int func(int m){
+        if(m<=1)return m;
+        return func(m-1)+func(m-2);
     }
 }
